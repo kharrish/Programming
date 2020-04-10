bcf749b463cb60cfa6ab37d0a6f0f2795765ecd1
kharrish <47523265+kharrish@users.noreply.github.com>
2020-04-10 20:31:35 +0530

    created student mark list using is-else-if
    The file prints the grade of the students based on the marks based on if-else-if statement.

---
+++ student.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+ int num;
+ printf("enter your mark");
+ scanf("%d",&num);
+ if(num>=85);
+ {
+ printf("you got A grade");
+ }
+ else if(num>=70);
+ {
+ printf("you got B grade");
+ }
+ else if(num>=55);
+ {
+ printf("you got C grade");
+ }
+ else if(num>=40);
+ {
+ printf("you got D grade");
+ }
+ else
+ {
+ printf("you got F grade");
+ }
+ return 0;
+ }
